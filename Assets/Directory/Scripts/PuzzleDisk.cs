6c7a9c6ab7abf6e7e2c0e5d77c0e583429017036
Varaygue <38994398+Varaygue@users.noreply.github.com>
2023-11-21 15:29:35 +0100

    Test_Disque
    Test_Disque

---
+++ Assets/Directory/Scripts/PuzzleDisk.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleDisk : MonoBehaviour
+{
+    public bool inZone = false;
+    public GameObject puzzlePanel;
+    public GameObject cameraPlayer;
+    public GameObject cameraPuzzle;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(inZone==true&&Input.GetKeyDown(KeyCode.F))
+        {
+            puzzlePanel.SetActive(true);
+            cameraPlayer.SetActive(false);
+            cameraPuzzle.SetActive(true);
+            Cursor.visible=true;
+            Cursor.lockState = CursorLockMode.Confined;
+        }
+        else if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            puzzlePanel.SetActive(false);
+            cameraPlayer.SetActive(true);
+            cameraPuzzle.SetActive(false);
+            Cursor.visible=false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider PuzzleZone)
+    {
+        inZone = true;
+    }
+    private void OnTriggerExit(Collider PuzzleZone)
+    {
+        inZone = false;
+    }
+}
